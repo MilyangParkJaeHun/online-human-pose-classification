465efd57691dfb27782f553197155463fc0d99c8
MilyangParkJaeHun <jh9277@naver.com>
2021-10-27 12:42:25 +0000

    Change argument parser elements

--- kps_extraction/Openvino.py
+++ kps_extraction/Openvino.py
@@ -37,17 +37,9 @@ def build_argparser():
     parser = ArgumentParser(add_help=False)
     args = parser.add_argument_group('Options')
     args.add_argument('-h', '--help', action='help', default=SUPPRESS, help='Show this help message and exit.')
-    args.add_argument('-m', '--model', help='Required. Path to an .xml file with a trained model.',
-                      required=True, type=Path)
-    args.add_argument('-at', '--architecture_type', help='Required. Specify model\' architecture type.',
-                      type=str, required=True, choices=('ae', 'openpose'))
     args.add_argument('-i', '--input', required=True, type=int,
                       help='Required. An input to process. The input must be a single image, '
                            'a folder of images, video file or camera id.')
-    args.add_argument('-d', '--device', default='CPU', type=str,
-                      help='Optional. Specify the target device to infer on; CPU, GPU, FPGA, HDDL or MYRIAD is '
-                           'acceptable. The sample will look for a suitable plugin for device specified. '
-                           'Default value is CPU.')
 
     common_model_args = parser.add_argument_group('Common model options')
     common_model_args.add_argument('-t', '--prob_threshold', default=0.1, type=float,
@@ -62,16 +54,24 @@ def build_argparser():
                                         'the target size in this case. For Associative Embedding-like nets target size '
                                         'is the length of a short first image side.')
 
-    infer_args = parser.add_argument_group('Inference options')
-    infer_args.add_argument('-nireq', '--num_infer_requests', help='Optional. Number of infer requests',
+    openvino_infer_args = parser.add_argument_group('Openvino inference options')
+    openvino_infer_args.add_argument('-nireq', '--num_infer_requests', help='Optional. Number of infer requests',
                             default=1, type=int)
-    infer_args.add_argument('-nstreams', '--num_streams',
+    openvino_infer_args.add_argument('-nstreams', '--num_streams',
                             help='Optional. Number of streams to use for inference on the CPU or/and GPU in throughput '
                                  'mode (for HETERO and MULTI device cases use format '
                                  '<device1>:<nstreams1>,<device2>:<nstreams2> or just <nstreams>).',
                             default='', type=str)
-    infer_args.add_argument('-nthreads', '--num_threads', default=None, type=int,
+    openvino_infer_args.add_argument('-nthreads', '--num_threads', default=None, type=int,
                             help='Optional. Number of threads to use for inference on CPU (including HETERO cases).')
+    openvino_infer_args.add_argument('-em', '--extractor_model', help='Required. Path to an .xml file with a trained model.',
+                      required=True, type=Path)
+    openvino_infer_args.add_argument('-at', '--architecture_type', help='Required. Specify model\' architecture type.',
+                      type=str, required=True, choices=('ae', 'openpose'))
+    openvino_infer_args.add_argument('-d', '--device', default='CPU', type=str,
+                      help='Optional. Specify the target device to infer on; CPU, GPU, FPGA, HDDL or MYRIAD is '
+                           'acceptable. The sample will look for a suitable plugin for device specified. '
+                           'Default value is CPU.')
 
     io_args = parser.add_argument_group('Input/output options')
     io_args.add_argument('-show_on', '--show_on', help="Optional. Show output.", action='store_true')
@@ -172,7 +172,7 @@ class Openvino():
             Model = models.OpenPose
         else:
             raise RuntimeError('No model type or invalid model type (-at) provided: {}'.format(args.architecture_type))
-        return Model(ie, args.model, target_size=args.tsize, aspect_ratio=aspect_ratio, prob_threshold=args.prob_threshold)
+        return Model(ie, args.extractor_model, target_size=args.tsize, aspect_ratio=aspect_ratio, prob_threshold=args.prob_threshold)
 
 
     def get_plugin_configs(self, device, num_streams, num_threads):
