986ee434215b8a555016092fae86aeb58134d8d8
MilyangParkJaeHun <jh9277@naver.com>
2021-12-13 12:22:04 +0000

    Change keypoints extractor openvino to openpose [whale version]

--- online_pose_classification.py
+++ online_pose_classification.py
@@ -12,8 +12,6 @@ from argparse import ArgumentParser, SUPPRESS
 import cv2
 import numpy as np
 
-from kps_extraction.Openvino import Openvino
-from kps_extraction.Openvino import build_argparser as openvino_args
 from kps_extraction.data_handler import DataHandler
 from pose_classification.pose_classifier import PoseClassifier
 
@@ -40,25 +38,6 @@ def build_argparser():
                                         'the target size in this case. For Associative Embedding-like nets target size '
                                         'is the length of a short first image side.')
 
-    openvino_infer_args = parser.add_argument_group('Openvino inference options')
-    openvino_infer_args.add_argument('-nireq', '--num_infer_requests', help='Optional. Number of infer requests',
-                            default=1, type=int)
-    openvino_infer_args.add_argument('-nstreams', '--num_streams',
-                            help='Optional. Number of streams to use for inference on the CPU or/and GPU in throughput '
-                                 'mode (for HETERO and MULTI device cases use format '
-                                 '<device1>:<nstreams1>,<device2>:<nstreams2> or just <nstreams>).',
-                            default='', type=str)
-    openvino_infer_args.add_argument('-nthreads', '--num_threads', default=None, type=int,
-                            help='Optional. Number of threads to use for inference on CPU (including HETERO cases).')
-    openvino_infer_args.add_argument('-em', '--extractor_model', help='Required. Path to an .xml file with a trained model.',
-                      required=True, type=Path)
-    openvino_infer_args.add_argument('-at', '--architecture_type', help='Required. Specify model\' architecture type.',
-                      type=str, required=True, choices=('ae', 'openpose'))
-    openvino_infer_args.add_argument('-d', '--device', default='CPU', type=str,
-                      help='Optional. Specify the target device to infer on; CPU, GPU, FPGA, HDDL or MYRIAD is '
-                           'acceptable. The sample will look for a suitable plugin for device specified. '
-                           'Default value is CPU.')
-
     clf_infer_args = parser.add_argument_group('Pose classifier inference options')
     clf_infer_args.add_argument('-cm', '--classifier_model', help='Required. Path to an .pkl file with a trained model.',
                       required=True, type=Path)
@@ -74,27 +53,24 @@ def build_argparser():
     return parser
 
 class OnlinePoseClassfier():
-    def __init__(self, kps_extractor: Openvino, \
-                    pose_classifier: PoseClassifier, \
+    def __init__(self, pose_classifier: PoseClassifier, \
                     data_handler: DataHandler, \
                     prob_threshold: float):
-        self.kps_extractor = kps_extractor
         self.pose_classifier = pose_classifier
         self.data_handler = data_handler
 
         self.prob_threshold = prob_threshold
         self.pose_buf = [0] * 8
 
-    def inference(self, frame: np.ndarray):
+    def inference(self, kps_list: list):
         """
         After estimating human pose in the camera frame,
         results of estimating human pose is stored in pose buffer.
         """
-        kps_list = self.kps_extractor.inference(frame)
         for kps in kps_list:
             if self.isNotValid(kps, self.prob_threshold):
                 continue
-            kps = self.convert_to_openpose(kps)
+            # kps = self.convert_to_openpose(kps)
             kps = self.data_handler.preprocess_data(kps)
 
             pose = self.pose_classifier.predict(kps)
@@ -131,10 +107,8 @@ class OnlinePoseClassfier():
         """
         Check if kepoints has unvalid kepoint data.
         """
-        check_list = [0, 5, 6, 7, 8, 9, 10, 11, 12]
-        for idx in check_list:
-            d = data[idx]
-            if d[0] == 0.0 or d[1] == 0.0 or d[2] < prob_threshold:
+        for d in data:
+            if d == 0.0:
                 return True
         return False
 
@@ -159,50 +133,3 @@ class OnlinePoseClassfier():
         if self.pose_buf[most_pose] > 0:
             return most_pose
         return normal_pose
-
-if __name__ == "__main__":
-    args = build_argparser().parse_args()
-    input_stream = args.input
-    prob_threshold = args.prob_threshold
-    interval = args.interval
-
-    cap_width = 640
-    cap_height = 480
-    cap = cv2.VideoCapture(input_stream)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, cap_width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, cap_height)
-
-    # Create human keypoints extractor instance
-    kps_extractor = Openvino(args, [cap_height, cap_width])
-
-    # Create human keypoints data handler instance
-    data_handler = DataHandler()
-
-    # Create human pose classifier
-    pose_classifier = PoseClassifier(args.classifier_model)
-
-    # Create online human pose classifier
-    online_pose_clf = OnlinePoseClassfier(kps_extractor, pose_classifier, data_handler, prob_threshold)
-
-    start_time = perf_counter()
-    most_pose = 0
-    while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        online_pose_clf.inference(frame)
-
-        if perf_counter() - start_time > interval:
-            most_pose = online_pose_clf.get_most_pose()
-            start_time = perf_counter()
-
-        cv2.putText(frame, pose_classifier.to_str(most_pose), (50, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2, cv2.LINE_AA)
-
-        cv2.imshow('pose classification', frame)
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            break
-
-    cv2.destroyAllWindows()
-    sys.exit(0)
--- ros/online_pose_classification/scripts/pose_classification_node.py
+++ ros/online_pose_classification/scripts/pose_classification_node.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 """
     pose_classification_node.py
     Author: Park Jaehun
@@ -7,30 +8,43 @@
         data type : Int16
         name : /manta/vision/pose
 """
-#!/usr/bin/env python3
-
 # pose list : /path/to/online_pose_classification/config
 
 import os
 import sys
 import logging
 import cv2
+import numpy as np
 from typing import List
 from time import perf_counter
 
 POSE_CLF_PATH=os.environ["POSE_CLF_PATH"]
 sys.path.append(POSE_CLF_PATH)
 
-from kps_extraction.Openvino import Openvino
 from kps_extraction.data_handler import DataHandler
 from pose_classification.pose_classifier import PoseClassifier
 from online_pose_classification import build_argparser
 from online_pose_classification import OnlinePoseClassfier
+from openpose_ros_msgs.msg import OpenPoseHumanList
+from openpose_ros_msgs.msg import OpenPoseHuman
 
 import rospy
 from std_msgs.msg import Int16
 
-def main(pub: rospy.Publisher) -> None:
+args = build_argparser().parse_args()
+prob_threshold = args.prob_threshold
+interval = args.interval
+
+data_handler = DataHandler()
+pose_classifier = PoseClassifier(model_path=args.classifier_model)
+online_pose_clf = OnlinePoseClassfier(pose_classifier, data_handler, prob_threshold)
+
+start_time = perf_counter()
+
+frame = None
+pub = rospy.Publisher('/manta/vision/pose', Int16, queue_size=10)
+
+def callback(data):
     """
     Run main loop
     1. Get camera frame
@@ -39,71 +53,33 @@ def main(pub: rospy.Publisher) -> None:
     4. Find most detected human pose
     5. Publish most detected human pose
     """
-    args = build_argparser().parse_args()
-    show_on = args.show_on
-    input_stream = args.input
-    prob_threshold = args.prob_threshold
-    interval = args.interval
+    global show_on, interval, online_pose_clf, start_time, pub
 
-    cap_width = 640
-    cap_height = 480
-    cap = cv2.VideoCapture(input_stream)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, cap_width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, cap_height)
+    if len(data.human_list) == 0:
+        return
 
-    # Create human keypoints extractor instance
-    kps_extractor = Openvino(args, [cap_height, cap_width])
+    kps_list = list()
+    for human in data.human_list:
+        kps_prob = human.body_key_points_with_prob[:9]
+        kps = list()
+        for kp in kps_prob:
+            kps.append(kp.x)
+            kps.append(kp.y)
+        kps_list.append(kps)
 
-    # Create human keypoints data handler instance
-    data_handler = DataHandler()
+    online_pose_clf.inference(kps_list)
 
-    # Create human pose classifier
-    pose_classifier = PoseClassifier(model_path=args.classifier_model)
+    if perf_counter() - start_time > interval:
+        most_pose = online_pose_clf.get_most_pose()
 
-    # Create online human pose classifier
-    online_pose_clf = OnlinePoseClassfier(kps_extractor, pose_classifier, data_handler, prob_threshold)
+        pose_msgs = Int16()
+        pose_msgs.data = most_pose
+        pub.publish(pose_msgs)
 
-    pose_msgs = Int16()
-    pose_buf = [0] * 8
-    most_pose = 7
-
-    start_time = perf_counter()
-    while not rospy.is_shutdown() and cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            rospy.loginfo("Could not read camera...")
-            break
-        
-        frame = cv2.flip(frame, 0)
-        frame = cv2.flip(frame, 1)
-
-        online_pose_clf.inference(frame)
-
-        if perf_counter() - start_time > interval:
-            most_pose = online_pose_clf.get_most_pose()
-
-            pose_msgs.data = most_pose
-            pub.publish(pose_msgs)
-
-            start_time = perf_counter()
-
-        if show_on:
-            cv2.putText(frame, pose_classifier.to_str(most_pose), (50, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 0), 2, cv2.LINE_AA)
-            cv2.imshow('pose classification', frame)
-            key = cv2.waitKey(1)
-            if key == ord('q'):
-                cv2.destroyAllWindows()
-                break
-                
-    cap.release()
+        start_time = perf_counter()
 
 if __name__ == "__main__":
-    rospy.init_node('oneline_pose_classification_node', anonymous=True)
-    
-    pub = rospy.Publisher('/manta/vision/pose', Int16, queue_size=10)
+    rospy.init_node('online_pose_classification_node', anonymous=True)
 
-    try:
-        main(pub)
-        sys.exit(0)
-    except rospy.ROSInterruptException:
-        sys.exit(1)
+    rospy.Subscriber('/openpose_ros/human_list', OpenPoseHumanList, callback)
+    rospy.spin()
